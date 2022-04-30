0daeb6786a2ea6f001d8943dd3386274008485fb
Daniel Workman <dworkman@squiz.net>
2022-04-30 12:12:58 +1000

    setting up framework for tickets page

---
+++ client/src/components/Tickets/getTickets.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tickets = () => {
+  return <div>tickets</div>;
+};
+
+export default Tickets;
---
+++ client/src/components/Tickets/updateSingleTicket.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpdateTicket = () => {
+  return <div>Single Tickets</div>;
+};
+
+export default UpdateTicket;
