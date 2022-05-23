b1f44cbe72c05b89633062cce3b8104a80564db6
Daniel Workman <dworkman@squiz.net>
2022-05-24 09:42:21 +1000

    ticket refactor

--- client/src/styled/styled.tsx
+++ client/src/styled/styled.tsx
@@ -1,38 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export const NavbarStyling = styled.div`
-    display: flex;
-    align-items: center;
-    max-width: 1920px;
-    width: 100%;
-    background-color: black;
-    color: white;
-    height: 100px;
-    padding-left: 10%;
-    padding-right: 10%;
-`
+  display: flex;
+  align-items: center;
+  max-width: 1920px;
+  width: 100%;
+  background-color: black;
+  color: white;
+  height: 100px;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
 
 export const NavTitles = styled.h3`
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    font-size: 24px;
+  font-size: 24px;
+  color: white;
+  padding-right: 20px;
+  a {
     color: white;
-    padding-right: 20px;
-    a {
-        color: white;
-    }
-`
+  }
+`;
 
 export const WebsiteMargin = styled.div`
-padding-left: 10%;
-padding-right: 10%;
-`
+  padding-left: 10%;
+  padding-right: 10%;
+`;
 
 export const NavFlexEnd = styled.div`
-display: flex;
-justify-content: flex-end;
-`
-
-// export const AuthForm = styled.div`
-// display: flex;
-// `
+  display: flex;
+  justify-content: flex-end;
+`;
